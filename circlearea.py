dcc9d0b5dd1a8771b884b3cec9c5106bac0eff51
Tanmay Jha <tanmayjha7668@proton.me>
2023-10-12 11:50:53 +0530

    Add files via upload

---
+++ circlearea.py
@@ -0,0 +1,6 @@
+#8
+radius=float(input("Enter radius of circle: "))
+area=3.17*radius*radius
+circu=2*31.4*radius
+print(f"The area of circle having radius {radius} is {area}")
+print(f"The circumference of circle having radius {radius} is {circu}")
---
+++ multiplicaationtable.py
@@ -0,0 +1,5 @@
+a=int(input("Enter the number: "))
+u=1
+while u<=10:
+    print(f"{a:} X {u} = {a*u}")
+    u +=1
